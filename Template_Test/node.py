711d0f1b5aeb4ea80960576e37b465f938f6e97c
huangdongdong <308997460@qq.com>
2020-04-08 22:15:28 +0800

    模板引擎

--- Template_Test/node.py
+++ Template_Test/node.py
@@ -2,12 +2,56 @@
 # -*- coding: utf-8 -*-
 
 # 导入系统模块
-
+import re
 # 第三方模块
-from .variable import Variable
 # 导入自定义模块
 
 # 设置环境变量
+
+
+
+class Variable:
+    """
+    作用：A template variable, resolvable against a given context. The variable may
+    be a hard-coded string (if it begins and ends with single or double quote
+    marks)::
+    >>> a = 'test'
+    >>> Variable('').resolve(a)
+    >>> 'test'
+    >>> b = [1, 2, 3]
+    >>> Variable('[1]').resolve(b)
+    >>> 2
+    >>> c = {'article': {'section':'News'}}
+    >>> Variable('article.section').resolve(c)
+    'News'
+    >>> Variable('article').resolve(c)
+    {'section': 'News'}
+    >>> d = {'book': {'language': ['Chinese', 'English']}}
+    >>> Variable('book.language[1]').resolve(d)
+    >>> 'English'
+    (The example assumes VARIABLE_ATTRIBUTE_SEPARATOR is '.')
+    """
+
+    def __init__(self, var_str):
+        self.var_str = None
+
+        if isinstance(var_str, str):
+            self.var_str = var_str
+        else:
+            raise TypeError("Variable must be a string or number, got %s" % type(var_str))
+
+    def resolve(self, context):
+        var_list = re.split(r'[.\[\]]', self.var_str)
+        value = context
+        for var in var_list:
+            if re.fullmatch(r'-?\d+', var):
+                value = value[int(var)]
+            elif re.fullmatch(r'\w+', var):
+                value = value[var] if var in value else 'NotFound'
+            else:
+                continue
+
+        return value
 
 
 class Node:
@@ -89,9 +133,20 @@ class ForNode(Node):
         if len_values < 1:
             pass
 
+        num_loop_vars = len(self.loop_vars)
+        unpack = num_loop_vars > 1
+
+        bits = []
         for i, item in enumerate(values):
-            self.nodelist_loop.render(item)
-            pass
+            if unpack:
+                unpacked_vars = dict(zip(self.loop_vars, item))
+                context.update(unpacked_vars)
+            else:
+                context[self.loop_vars[0]] = item
+
+            bits.append(self.nodelist_loop.render(context))
+
+        return ''.join(bits)
 
 
 class NodeList(list):
@@ -111,3 +166,12 @@ class NodeList(list):
 
 
 
+if __name__ == '__main__':
+    a = 'test'
+    print(Variable('').resolve(a))
+    b = [1, 2, 3]
+    print(Variable('[1]').resolve(b))
+    c = {'article': {'section': 'News'}}
+    print(Variable('article.section').resolve(c))
+    d = {'book': {'language': ['Chinese', 'English']}}
+    print(Variable('book.language[1]').resolve(d))
--- Template_Test/variable.py
+++ Template_Test/variable.py
@@ -4,6 +4,7 @@
 # 导入系统模块
 import re
 
+
 # 第三方模块
 
 # 导入自定义模块
@@ -14,24 +15,23 @@ import re
 class Variable:
     """
     作用：A template variable, resolvable against a given context. The variable may
-        be a hard-coded string (if it begins and ends with single or double quote
-        marks)::
-        >>> a = 'test'
-        >>> Variable('').resolve(a)
-        >>> 'test'
-        >>> b = [1, 2, 3]
-        >>> Variable('[1]').resolve(b)
-        >>> 2
-        >>> c = {'article': {'section':'News'}}
-        >>> Variable('article.section').resolve(c)
-        'News'
-        >>> Variable('article').resolve(c)
-        {'section': 'News'}
-        >>> d = {'book': {'language': ['Chinese', 'English']}}
-        >>> Variable('book.language[1]').resolve(d)
-        >>> 'English'
-
-        (The example assumes VARIABLE_ATTRIBUTE_SEPARATOR is '.')
+    be a hard-coded string (if it begins and ends with single or double quote
+    marks)::
+    >>> a = 'test'
+    >>> Variable('').resolve(a)
+    >>> 'test'
+    >>> b = [1, 2, 3]
+    >>> Variable('[1]').resolve(b)
+    >>> 2
+    >>> c = {'article': {'section':'News'}}
+    >>> Variable('article.section').resolve(c)
+    'News'
+    >>> Variable('article').resolve(c)
+    {'section': 'News'}
+    >>> d = {'book': {'language': ['Chinese', 'English']}}
+    >>> Variable('book.language[1]').resolve(d)
+    >>> 'English'
+    (The example assumes VARIABLE_ATTRIBUTE_SEPARATOR is '.')
     """
 
     def __init__(self, var_str):
@@ -39,18 +39,17 @@ class Variable:
 
         if isinstance(var_str, str):
             self.var_str = var_str
-            pass
         else:
             raise TypeError("Variable must be a string or number, got %s" % type(var_str))
 
-    def resolve(self, variable):
+    def resolve(self, context):
         var_list = re.split(r'[.\[\]]', self.var_str)
-        value = variable
+        value = context
         for var in var_list:
             if re.fullmatch(r'-?\d+', var):
                 value = value[int(var)]
             elif re.fullmatch(r'\w+', var):
-                value = value[var]
+                value = value[var] if var in value else 'NotFound'
             else:
                 continue
 
